daca88d25985894a2fde0fcd6c3ffe07105343cf
Matt Cotter <mcotter222@gmail.com>
2014-11-02 04:22:15 -0600

    Make services searchable / find hot/recent -able

--- static/index.js
+++ static/index.js
@@ -1,27 +1,53 @@
 $(document).ready(function () {
-	var prev = null;
-	$('#main .service').each(function(){
-		var classes = ['red', 'green', 'yellow', 'purple', 'darkblue', 'turquoise','emerald','blue','belize'];
-		var i;
-		while(true){
-			i = Math.floor(Math.random() * classes.length);
-			if (i != prev) { break };
-		}
-		prev=i;
-		$(this).addClass(classes[i]);
+	function colors () {
+		var prev = null;
+		$('#main .service').each(function(){
+			var classes = ['red', 'green', 'yellow', 'purple', 'darkblue', 'turquoise','emerald','blue','belize'];
+			var i;
+			while(true){
+				i = Math.floor(Math.random() * classes.length);
+				if (i != prev) { break };
+			}
+			prev=i;
+			$(this).addClass(classes[i]);
+		});
+	}
+	colors();
+
+	$('#nav-search').keydown(function(){
+		$( "#content" ).load( "/search?search="+$('#nav-search').val(), function() {
+			console.log("loaded script successfully");
+			colors();
+		} );
 	});
 
-	$('.about').click(function(){
+	$('#nav-hot').click(function(){
+		$( "#content" ).load( "/hot", function() {
+			console.log("loaded script successfully");
+			colors();
+		} );
+		return false;
+	});
+
+	$('#nav-recent').click(function(){
+		$( "#content" ).load( "/recent", function() {
+			console.log("loaded script successfully");
+			colors();
+		} );
+		return false;
+	});
+
+	$('#nav-about').click(function(){
 		$( "#content" ).load( "/static/about.html", function() {
 			console.log("loaded script successfully");
 		} );
 		return false;
 	});
 
-	$('.create').click(function(){
+	$('#nav-create').click(function(){
 		$( "#content" ).load( "/create", function() {
 			console.log("loaded script successfully");
 		} );
 		return false;
-	})
+	});
 });
